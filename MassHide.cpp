43470b3c4d31eb53411574e7a7122582e277f836
Daniel R. Collins ("Delta") <dcollins@superdan.net>
2021-01-15 00:24:35 -0500

    Add files via upload

--- MassHide.cpp
+++ MassHide.cpp
@@ -69,5 +69,5 @@ int main(int argc, char *argv[])
 	srand(time(0));
 	float spotRatio = manyTrials();
 	std::cout << "Spot success: " << spotRatio * 100 << "%\n";
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
